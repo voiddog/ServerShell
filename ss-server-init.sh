d0f607c850630147ba28a63ff5287fa0b002f11b
voiddog <voiddog@foxmail.com>
2017-11-28 14:58:15 +0800

    add ss-lib install shell

--- ss-server-init.sh
+++ ss-server-init.sh
@@ -9,7 +9,8 @@ chmod 600 /root/.ssh
 echo ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDikRSQnx1gqlyynTAl0bNc8Dvm4oLqHwmIdhdNnju6VAC3DATT7H2J6cm7O6RbaUX7swFaqRSV7+7Ay6jd97MeIv3/j0/KyW3BnK5HTE2mQuDtdXrHippUJsLrplh+gYUDhIccDmX0lLL4mnR+Tb9WG7OAvzY425o9YwrXyNrYvBuhAc0clo1x5RXEuTubINU8L1TeDgbbd21GwwBNem4yv2IsrTSaFlon8tePxWliESAWJpPBGziW2RLwLtOJ5xY63OxuRpBRJn/iCFys67eMg9kQHUs2s3edrJEwrTsUpqsHsHcgzmEwEfGJlQtle8K+iLNSlD06Qhe6T5amUxFp voiddog@foxmail.com > /root/.ssh/authorized_keys
 chmod 700 /root/.ssh/authorized_keys
 
-yum install git vim zsh -y
+yum install git vim zsh wget -y
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
-wget -O /etc/yum.repos.d/librehat-shadowsocks-epel-7.repo https://copr.fedorainfracloud.org/coprs/librehat/shadowsocks/repo/epel-7/librehat-shadowsocks-epel-7.repo
-yum install shadowsocks-libev -y
+wget --no-check-certificate -O shadowsocks-libev.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-libev.sh
+chmod +x shadowsocks-libev.sh
+./shadowsocks-libev.sh 2>&1 | tee shadowsocks-libev.log
